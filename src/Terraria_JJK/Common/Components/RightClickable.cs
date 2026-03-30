ee8e39a4dee110ffc7ed852f445ef8484f6e47ae
TacoConKvass <e_frun@o2.pl>
2026-03-31 00:04:27 +0200

    Fix: `RightClickable` component for Items

--- src/Terraria_JJK/Common/Components/RightClickable.cs
+++ src/Terraria_JJK/Common/Components/RightClickable.cs
@@ -1,21 +1,16 @@
 namespace Terraria_JJK.Components;
 
 [EC.Component]
-public struct RightClickable
+public record struct RightClickable(System.Func<Terraria.Entity, bool> When, System.Func<Terraria.Player, bool?> Effect)
 {
-	public System.Action<Terraria.Player> Effect;
-}
-
-internal class RightClickable_Impl
-{
-	[DaybreakHooks.GlobalItemHooks.CanRightClick]
-	internal static bool Item_RightClickable(DaybreakHooks.GlobalItemHooks.CanRightClick.Original orig, Terraria.Item item) {
-		return item.Enabled<RightClickable>() ? true : orig(item);
+	[DaybreakHooks.GlobalItemHooks.AltFunctionUse]
+	internal static bool Item_RightClickable(DaybreakHooks.GlobalItemHooks.AltFunctionUse.Original orig, Terraria.Item item, Terraria.Player player) {
+		return item.TryGet(out RightClickable data) ? data.When(player) : orig(item, player);
 	}
 
-	[DaybreakHooks.GlobalItemHooks.RightClick]
-	internal static void Item_TriggerEffect(Terraria.Item item, Terraria.Player player) {
-		if (!item.TryGet<RightClickable>(out var data)) return;
-		data.Effect(player);
+	[DaybreakHooks.GlobalItemHooks.UseItem]
+	internal static bool? Item_TriggerEffect(Terraria.Item item, Terraria.Player player) {
+		if (!item.TryGet<RightClickable>(out var data) || player.altFunctionUse != 2) return null;
+		return data.Effect?.Invoke(player);
 	}
 }

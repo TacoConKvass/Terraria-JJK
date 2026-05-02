0ef1c48628fb07fd98f88a01190efd496e7c3612
TacoConKvass <e_frun@o2.pl>
2026-05-02 13:30:16 +0200

    feat(content): improve Kamutoke lightning behaviour

--- src/Terraria_JJK/Content/Kamutoke.cs
+++ src/Terraria_JJK/Content/Kamutoke.cs
@@ -6,7 +6,7 @@ public class Kamutoke : TML.ModItem
 {
 	public override string Texture => Terraria_JJK.AssetPath($"Items/{nameof(Kamutoke)}");
 
-	const float LightningSpeed = 1f;
+	const float LightningSpeed = 2f;
 
 	public override void SetDefaults() {
 		Item.Size = new FNA.Vector2 { X = 58, Y = 58 };
@@ -29,20 +29,28 @@ public class KamutokeLightning : TML.ModProjectile
 
 	public static int ID => TML.ModContent.ProjectileType<KamutokeLightning>();
 
+	const int TicksOfMovementPerStraight = 20;
+	const int Turns = 15;
+
+	ref float LevelInHierarchy(Terraria.Projectile projectile) => ref projectile.ai[0];
+	ref float SpawnedChildrenCount => ref Projectile.ai[1];
+	ref float TurnDirection => ref Projectile.ai[2];
+
 	public override void SetDefaults() {
 		Projectile.Size = new FNA.Vector2 { X = 16, Y = 16 };
 		Projectile.friendly = true;
-		Projectile.extraUpdates = 20;
-		Projectile.timeLeft = 30 * Projectile.extraUpdates;
+		Projectile.extraUpdates = TicksOfMovementPerStraight;
+		Projectile.timeLeft = Turns * TicksOfMovementPerStraight;
 		Projectile.usesLocalNPCImmunity = true;
 		Projectile.localNPCHitCooldown = -1;
+		Projectile.penetrate = 1;
 
 		Projectile.With(new Components.Trail {
 			MaxPositions = Projectile.timeLeft,
 			Width = static (progress) => progress > 0 && progress < 1 ? 2 : 0,
 			Color = static (_) => FNA.Color.Aqua,
-			Delay = Projectile.extraUpdates * 2,
-			FadeDelay = 1,
+			Delay = TicksOfMovementPerStraight,
+			FadeSpeed = 1,
 		});
 		Projectile.With(new Components.Trail.AddPosition { }); // Force first position to be added
 		Projectile.With(new Components.OnTimer<Components.SpawnProjectile> {
@@ -55,7 +63,7 @@ public class KamutokeLightning : TML.ModProjectile
 			},
 		});
 		Projectile.With(new Components.OnTimer<Components.ChangeVelocity> {
-			Timer = Projectile.extraUpdates * 2,
+			Timer = TicksOfMovementPerStraight,
 			Resettable = true,
 			Inner = new Components.ChangeVelocity {
 				Change = ChangeVelocity
@@ -63,42 +71,44 @@ public class KamutokeLightning : TML.ModProjectile
 		});
 	}
 
-	FNA.Vector2 ChildVelocity() => Projectile.velocity.RotatedBy(10 * (1 + Projectile.ai[1]) * FNA.MathHelper.TwoPi / 360f);
+	FNA.Vector2 ChildVelocity() => Projectile.velocity.RotatedBy(10 * LevelInHierarchy(Projectile) * FNA.MathHelper.TwoPi / 360f);
 
 	static float[] angles = [-1f, 0.5f, 1f];
 
 	FNA.Vector2 ChangeVelocity(FNA.Vector2 old) {
 		foreach (var npc in Terraria.Main.ActiveNPCs) {
-			if (Projectile.Center.DistanceSQ(npc.Center) < 64 * 64)
+			if (!npc.friendly && Projectile.Center.DistanceSQ(npc.Center) < 64 * 64)
 				return Projectile.DirectionTo(npc.Center);
 		}
 
 		if (Terraria.Main.netMode != Terraria.ID.NetmodeID.MultiplayerClient)
-			Projectile.ai[2] = angles[System.Math.Clamp((int)Projectile.ai[2] * -1, -1, 1) + 1] * Terraria.Main.rand.NextFloat(30, 45);
+			TurnDirection = angles[System.Math.Clamp((int)TurnDirection * -1, -1, 1) + 1] * Terraria.Main.rand.NextFloat(30, 45);
 
-		return old.RotatedBy(Projectile.ai[2] * FNA.MathHelper.TwoPi / 360f);
+		return old.RotatedBy(TurnDirection * FNA.MathHelper.TwoPi / 360f);
 	}
 
 	void ChildCallback(Terraria.Projectile child) {
-		if (Projectile.ai[1] > -1) {
-			Projectile.ai[1] = -2;
-			if (Terraria.Main.netMode != Terraria.ID.NetmodeID.MultiplayerClient)
-				Projectile.With(new Components.OnTimer<Components.SpawnProjectile> {
-					Timer = 15,
-					Inner = new Components.SpawnProjectile {
-						Type = ID,
-						Velocity = ChildVelocity,
-						Damage = Projectile.damage,
-						Callback = ChildCallback
-					},
-				});
+		if (SpawnedChildrenCount < 2) {
+			SpawnedChildrenCount++;
+			Projectile.With(new Components.OnTimer<Components.SpawnProjectile> {
+				Timer = 15,
+				Inner = new Components.SpawnProjectile {
+					Type = ID,
+					Velocity = ChildVelocity,
+					Damage = Projectile.damage,
+					Callback = ChildCallback
+				},
+			});
 		}
-		if (Projectile.ai[0] >= 1) {
+
+		child.timeLeft = Projectile.timeLeft;
+		LevelInHierarchy(child) = LevelInHierarchy(Projectile) + 1;
+		if (LevelInHierarchy(Projectile) >= 1) {
 			child.Disable<Components.OnTimer<Components.SpawnProjectile>>();
 			return;
 		}
-		child.ai[0] = Projectile.ai[0] + 1;
-		_ = child.TryGet(out Components.OnTimer<Components.SpawnProjectile> data);
+
+		child.TryGet(out Components.OnTimer<Components.SpawnProjectile> data);
 		child.Set(data with {
 			Timer = (int)(data.Timer * Terraria.Main.rand.NextFloat(0.75f, 3f))
 		});
